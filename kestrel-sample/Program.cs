6aba1a360b796f7236ffda766f7634e75f2c702e
George Kinsman <george.kinsman@ticketer.co.uk>
2018-08-02 12:53:47 +0100

    Repro project

---
+++ kestrel-sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace kestrel_sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(opt => {
+                    opt.Listen(IPAddress.Loopback, 5001, lopt => {
+                       lopt.UseHttps();
+                    });
+                });
+    }
+}
